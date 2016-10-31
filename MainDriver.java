7fe8caee98d76f12c85e149e0df55cc7258df48e
Sam Messina <Sam Messina>
2016-10-31 15:23:09 -0500

    added skeleton comment for analysis and compilation

--- MainDriver.java
+++ MainDriver.java
@@ -1,17 +1,28 @@
 public class MainDriver {
-        public static void main(String[] args) {
-                String originalSource = "for (int a = 0; a == 3; a++) { statement; statement;}";
-                String[] tokenArray;
+	public static void main(String[] args) {
+		String originalSource = "for (int a = 0; a == 3; a++) { statement; statement;}";
+		String[] tokenArray;
 
-                Parser source = new Parser(originalSource);
+		Parser source = new Parser(originalSource);
 
-                tokenArray = source.getTokens();
+		tokenArray = source.getTokens();
 
-                for (String token : tokenArray) {
-                        System.out.printf("Next token is %s\n", token);
-                }
+		for (String token : tokenArray) {
+			System.out.printf("Next token is %s\n", token);
+		}
 
+		/*
 
-        }
+		   - Lexical Analysis -
+		   LexicalAnalyzer le = new LexicalAnalyzer(tokenArray);
+		   le.analyze();
+
+		   - Compiler -
+		   OurCompiler oc = new OurCompiler(tokenArray);
+		   new.run();
+
+		 */
+
+	}
 
 }
