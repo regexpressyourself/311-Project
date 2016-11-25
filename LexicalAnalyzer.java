2c83c45e19ce988a5eb2465f9790115bd9422882
Sam Messina <samuel.messina@gmail.com>
2016-11-24 21:18:16 -0600

    Added handleIf() method and updated QueueImplementation calls
    handleIf() will now work to execute the line following if statements only if the if statement evaluates to true. If statements can only recieve "EMPTY" or "NOT_EMPTY" as parameters, or else syntaxError() will be called.
    
    Also added: new calls for different commands that correspond with the new QueueImplementation methods.
    
    TODO: Implement syntaxError() method
    TODO: error check source code and call syntaxError() where needed
    
    Happy thanksgiving!

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,9 +1,9 @@
 import java.util.*;
 import java.io.*;
 public class LexicalAnalyzer{
-    String[] tokenArray;
-    QueueImplementation queue = new QueueImplementation();
-    static i = 0;
+    static String[] tokenArray;
+    static QueueImplementation<Integer> queue = new QueueImplementation<Integer>();
+    static int i = 0;
 
     public LexicalAnalyzer(String[] tokenArray) {
         this.tokenArray = tokenArray;
@@ -22,34 +22,59 @@ public class LexicalAnalyzer{
                 queue.remove();
                 break;
             case "PEEK":
-                queue.peek();
+                queue.showFirst((Integer) queue.peek());
                 break;
             case "LENGTH":
-                System.out.println("Queue size: " + queue.size());
+                queue.getLength(queue.size());
                 break;
             case "EMPTY":
+                queue.isEmpty();
                 break;
             case "NOT_EMPTY":
+                queue.notEmpty();
                 break;
             case "VIEW":
+                queue.view();
                 break;
             case "IF":
                 handleIf();
+                break;
+            case "CLEAR":
+                queue.clear();
                 break;
             default:
                 break;
 
             }
-            removeSemiColon(i);
-        }
-    }
-
-    public static void removeSemiColon() {
-        if (tokenArray[i+1].equals(";")) {
-            i += 1;
-        }
+            if (tokenArray[i+1].equals(";")) {
+                i += 1;
+            }
+        } // end for
     }
     public static void handleIf() {
-    }
+        while (!tokenArray[i].equals("(")){ i++; }
+        i++;
 
+        if (tokenArray[i].equals("EMPTY") && tokenArray[i+2].equals(";")){
+            if(queue.isEmpty()) {
+                i += 1;
+            }
+            else {
+                i += 3;
+            }
+        }
+        else if (tokenArray[i].equals("NOT_EMPTY") && tokenArray[i+2].equals(";")){
+            if(queue.notEmpty()) {
+                i += 1;
+            }
+            else {
+                i += 3;
+            }
+        }
+        else {
+            syntaxError();
+        }
+    }
+    public static void syntaxError() {
+    }
 }
