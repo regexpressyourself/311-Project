6197a3ba5b3bb63a0d234a33560e73cab2827825
Sam Messina <samuel.messina@gmail.com>
2016-11-24 21:32:57 -0600

    reformat to create interpretted/compiled versions
    Total refactor. I've divided everything up into folders so we can move
    forward. Folders are arranged as follows:
    
    - documents -
    Contains progress report, BNF, and other pdf or markdown files
    
    - original -
    Contains the current, working version of the program. This is pretty
    much at a point that it can be turned in right now.
    
    - interpretted -
    Contains the version of our language that is interpretted, i.e.
    everything runs all at once. This version will require combining the
    functionality between all our classes into one.
    
    Note: interpretted is currently identical to original
    
    - compiled -
    Contains the version of our language that is compiled, i.e.
    exports to an executable file. This version will require writing our
    commands to a .java file, compiling the file down, and then saving the
    compiled version onto the disk.
    
    Note: compiled is currently identical to original

---
+++ original/QueueImplementation.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class QueueImplementation<E> extends ArrayDeque {
+
+    /**
+     * ArrayDeque has implementation of most of our queue methods for us. These include:
+     *
+     * add(element) - adds to end of queue
+     * remove()     - removes and returns first element in queue
+     * peek()       - returns but does not remove first element in queue
+     * size()       - returns the number of elements in the queue
+     */
+
+    public void view() {
+        String    prettyView = "";
+        Object[] arrayQueue = this.toArray();
+
+        for (int i = 0; i < arrayQueue.length; i++) {
+            prettyView += arrayQueue[i] + " <- ";
+        }
+        System.out.println(prettyView);
+    }
+
+    public void showFirst(Integer firstElement) {
+        System.out.println("The first element is: " + firstElement);
+
+    }
+    public void getLength(int length) {
+        System.out.println("The length is: " + length);
+    }
+
+    public boolean isEmpty() {
+        return (this.size() == 0);
+    }
+
+    public boolean notEmpty() {
+        return (this.size() > 0);
+    }
+}
