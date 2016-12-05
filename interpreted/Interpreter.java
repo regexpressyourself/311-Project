9319166e13ee53809a395e2b8ccca7e9bf2445bc
Sam Messina <samuel.messina@gmail.com>
2016-12-05 12:26:39 -0600

    Added comments to interpreted version

--- interpreted/Interpreter.java
+++ interpreted/Interpreter.java
@@ -1,58 +1,76 @@
 import java.util.*;
 
 public class Interpreter {
-    public static List<String> tokenArray;
-    public static String       nextToken;
+    public static QueueImplementation<Integer> queue = new QueueImplementation<Integer>();
+    // the QueueImplementation which will run our commands
 
-    public boolean      verbose;
-    public String       sourceCode;
-    public char[]       sourceArray;
-    public char         currentChar;
-    public String       finalTokenArray[];
+    public static String nextToken;   // the next token
+    public static int    ifArgCount;  // placeholder for parsing an if statement
 
-    static QueueImplementation<Integer> queue = new QueueImplementation<Integer>();
-    static int ifArgCount = 0;
+    public boolean       verbose;     // print tokens as they arrive
+    public String        sourceCode;  // the original Q Language source code
+    public char[]        sourceArray; // the original source in a char array
+    public char          currentChar; // the current character being parsed
+
 
     public Interpreter() {
+        // constructor with nothing
         this.sourceCode  = "";
         this.sourceArray = sourceCode.toCharArray();
         this.verbose     = true;
     }
     public Interpreter(String sourceCode) {
+        // constructor with source code
         this.sourceCode  = sourceCode;
         this.sourceArray = sourceCode.toCharArray();
         this.verbose     = true;
     }
     public Interpreter(String sourceCode, boolean verbose) {
+        // constructor with source code and verbose option set
+        this.sourceCode  = sourceCode;
         this.sourceCode  = sourceCode;
         this.sourceArray = sourceCode.toCharArray();
         this.verbose     = verbose;
     }
 
-    public String[] getTokens() {
+    public void getTokens() {
+        /**
+         * getTokens is the meat and potatoes of the interpreted version
+         *
+         * getTokens will run through every character in the source file, parsing, analyzing,
+         * and executing as it goes. It gets a little messy, but it works.
+         *
+         * The boolean flags help keep track of things like if statements,
+         * ignoring semi colons, adding the next token, etc.
+         *
+         * @param  none, but uses global variables in the class
+         * @return none, but runs the appropriate java code
+         */
 
         boolean executeIf       = false; // evaluate expression following if statement
         boolean inIf            = false; // in an if statement
         boolean addNextToken    = false; // triggered when the next token is an element to be added
         boolean ignoreNextToken = false; // flag to ignore semi colons
+        ifArgCount              = 0;     // set ifArgCount to 0
         nextToken               = "";
-        tokenArray              = new ArrayList<String>();
 
         for (int i = 0; i < sourceCode.length(); i++){
+            // for every token, analyze it and add the correct QueueImplementation method
             currentChar = sourceCode.charAt(i);
+
             if (!(Character.isSpaceChar(currentChar) || currentChar == '\n')) {
 
-                if (Character.isDigit(currentChar)) {
+                if (Character.isDigit(currentChar)) { // get number
                     nextToken = getNextNumber(sourceArray, i);
                     i        += nextToken.length() - 1;
                 } //end isDigit
 
-                else if (Character.isLetter(currentChar)) {
+                else if (Character.isLetter(currentChar)) { // get word
                     nextToken = getNextWord(sourceArray, i);
                     i        += nextToken.length() - 1;
                 } //end isLetter
 
-                else if (isSymbol(sourceArray, i)) {
+                else if (isSymbol(sourceArray, i)) { // get symbol
                     nextToken = getNextSymbol(sourceArray, i);
                     i        += nextToken.length() - 1;
                 }
@@ -64,20 +82,20 @@ public class Interpreter {
 
                 // Once the token is found, analyze it
 
-                if (verbose){
+                if (verbose) { // print the token if verbose option is set to true
                     System.out.println("Got token " + nextToken);
                 }
 
                 if (ignoreNextToken) {
                     // ignoreNextToken is triggered to ignore a semi colon
 
-                    if (!nextToken.equals(";")){
+                    if (!nextToken.equals(";")){ // check that semi colon is ignored
                         syntaxError();
                     }
 
                     ignoreNextToken = false;
                     if (inIf) {
-                        // reset if related flags
+                        // reset if related flags if the semi colon follows an if statement
                         ifArgCount = 0;
                         inIf       = false;
                         executeIf  = false;
@@ -126,23 +144,26 @@ public class Interpreter {
                         } // end if
                     } // end else if
                     else if (addNextToken ) {
+                        // catch when previous token was "ADD"
                         addNextToken    = false;
                         ignoreNextToken = true;
                         queue.add(nextToken);
                     }
                 } // end else if (inIf)
                 else if (addNextToken) {
-                  if (Integer.parseInt(nextToken) >= 0 && Integer.parseInt(nextToken) < 11) {
-                    addNextToken    = false;
-                    ignoreNextToken = true;
-                    queue.add(nextToken);
-                  }
-                  else {
-                    System.err.println("Queue elements must be integers from 0 - 10");
-                    syntaxError();
-                  }
+                    // catch when previous token was "ADD"
+                    if (Integer.parseInt(nextToken) >= 0 && Integer.parseInt(nextToken) < 11) {
+                        addNextToken    = false;
+                        ignoreNextToken = true;
+                        queue.add(nextToken);
+                    }
+                    else {
+                        System.err.println("Queue elements must be integers from 0 - 10");
+                        syntaxError();
+                    }
                 }
                 else {
+                    // if no flags are set, analysis starts here
                     switch (nextToken) {
                     case "ADD":
                         addNextToken = true;
@@ -177,18 +198,22 @@ public class Interpreter {
                         break;
                     } // end switch
                 } // end else
-                tokenArray.add(nextToken);
             }// end if(!is space)
         } //end for loop
 
-        finalTokenArray = new String[tokenArray.size()];
-        finalTokenArray = tokenArray.toArray(finalTokenArray);
-        return finalTokenArray;
 
     }// end getTokens
 
     public static boolean evaluateIf(String nextToken){
-        // evaluate the parameter of if statement and decide if it should execute or not
+        /**
+         * evaluate the parameter of if statement and decide if it should execute or not
+         *
+         * When an if statement is reached, evaluateIf() will be called to
+         * evaluate the statement in the parenthesis.
+         *
+         * @param  none, but it uses static variables nextToken, queue, and executeIf.
+         * @return whether or not the if statement evaluates to true
+         */
         boolean executeIf = false;
         switch (nextToken) {
         case "EMPTY":
@@ -215,12 +240,28 @@ public class Interpreter {
     }
 
     public static void syntaxError() {
-        // throw a syntax error on the next token
+        /**
+         * syntaxError throws an error and exits, displaying the last token analyzed
+         *
+         * @param  none, but it uses static variables i and tokenArray.
+         * @return none
+         */
+
         System.out.println("Syntax error on token: " + nextToken);
         System.exit(1);
     }
 
     public static boolean isSymbol(char[] charArray, int currentIndex){
+        /**
+         * isSymbol determines if the current character is a valid Q Language symbol
+         *
+         * Valid symbols in the Q Programming language are parentheses and semi-colons
+         *
+         * @param  charArray    - the array of characters in the source code
+         * @param  currentIndex - the placeholder for where the parser is in the array
+         * @return true if the character is a valid symbol, false otherwise
+         */
+
         switch (charArray[currentIndex]) {
         case '(':
         case ')':
@@ -232,6 +273,16 @@ public class Interpreter {
     } //end isSymbol
 
     public static String getNextSymbol(char[] charArray, int currentIndex) {
+        /**
+         * getNextSymbol parses the code to find the next symbol and returns it.
+         *
+         * Valid symbols in the Q Programming language are parentheses and semi-colons
+         *
+         * @param  charArray    - the array of characters in the source code
+         * @param  currentIndex - the placeholder for where the parser is in the array
+         * @return the symbol as a string
+         */
+
         String returnString = "";
         switch (charArray[currentIndex]) {
         case '(':
@@ -246,6 +297,14 @@ public class Interpreter {
     }
 
     public static String getNextWord(char[] charArray, int currentIndex){
+        /**
+         * getNextWord parses the code to find the next word and returns it.
+         *
+         * @param  charArray    - the array of characters in the source code
+         * @param  currentIndex - the placeholder for where the parser is in the array
+         * @return the word as a string
+         */
+
         String nextWord = "";
         while (Character.isLetter(charArray[currentIndex]) ||
                Character.isDigit (charArray[currentIndex])  ||
@@ -257,6 +316,14 @@ public class Interpreter {
     }
 
     public static String getNextNumber(char[] charArray, int currentIndex){
+        /**
+         * getNextNumber parses the code to find the next number and returns it.
+         *
+         * @param  charArray    - the array of characters in the source code
+         * @param  currentIndex - the placeholder for where the parser is in the array
+         * @return the number as a string
+         */
+
         String nextNum = "";
         while (Character.isDigit(charArray[currentIndex])){
             nextNum += charArray[currentIndex++];
--- interpreted/QueueImplementation.java
+++ interpreted/QueueImplementation.java
@@ -3,40 +3,71 @@ import java.util.*;
 public class QueueImplementation<E> extends ArrayDeque {
 
     /**
-     * ArrayDeque has implementation of most of our queue methods for us. These include:
+     * QueueImplementation is the object that will be manipulated by the Q Language code
      *
-     * add(element) - adds to end of queue
-     * remove()     - removes and returns first element in queue
-     * peek()       - returns but does not remove first element in queue
-     * size()       - returns the number of elements in the queue
+     * QueueImplementation makes use of Java's ArrayDequeue by extending it. No
+     * methods are overwritten, but some are added to make QueueImplementation work
+     * in line with the Q Programming Language.
      */
 
     public void view() {
-        // view the queue
-        String    prettyView = "";
-        Object[] arrayQueue = this.toArray();
+        /**
+         * view prints the queue in a readable way
+         * @param  none
+         * @return none
+         */
+
+        String   prettyView = "";             // The printed version of the queue
+        Object[] arrayQueue = this.toArray(); // The queue in array form
 
         for (int i = 0; i < arrayQueue.length; i++) {
             prettyView += arrayQueue[i] + " <- ";
         }
+
         System.out.println(prettyView);
     }
 
     public void showFirst(String firstElement) {
-        // our version of peek() that prints
+        /**
+         * showFirst is a version of peek that prints the first element rather than returning it
+         *
+         * @param  firstElement - the first element (attained by ArrayDeque's "peek()")
+         * @return none
+         */
+
         System.out.println("The first element is: " + firstElement);
 
     }
     public void getLength(int length) {
-        // our version of size() that prints
+        /**
+         * getLength is a version of size() that prints the length
+         *
+         * @param  the length (attained by ArrayDeque's "size()")
+         * @return none
+         */
+
         System.out.println("The length is: " + length);
     }
 
     public boolean isEmpty() {
+        /**
+         * isEmpty determines if the queue is empty
+         *
+         * @param  none
+         * @return true if empty, false if not
+         */
+
         return (this.size() == 0);
     }
 
     public boolean notEmpty() {
+        /**
+         * notEmpty determines if the queue is not empty
+         *
+         * @param  none
+         * @return true if not empty, false if empty
+         */
+
         return (this.size() > 0);
     }
 }
